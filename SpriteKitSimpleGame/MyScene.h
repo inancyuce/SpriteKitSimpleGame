8fda5c5444d660454ca4bda6dd2d9428cc59d00c
Inanc Yuce <inancyuce@Inancs-Mac-mini.local>
2013-12-21 04:21:49 +0200

    Initial Commit

---
+++ SpriteKitSimpleGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteKitSimpleGame
+//
+
+//  Copyright (c) 2013 YUCE. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ SpriteKitSimpleGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpriteKitSimpleGame
+//
+//  Created by Inanc Yuce on 21/12/13.
+//  Copyright (c) 2013 YUCE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
