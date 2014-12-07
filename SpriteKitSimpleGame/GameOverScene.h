0478cb1d7c8e2cd095427244c072c0f9a7743c4e
inancyuce <inancyuce@gmail.com>
2014-12-07 16:11:34 +0200

    Simple SpriteKit Game
    Learning swift

---
+++ SpriteKitSimpleGame/GameOverScene.h
@@ -0,0 +1,17 @@
+//
+//  GameOverScene.h
+//  SpriteKitSimpleGame
+//
+//  Created by Inanc Yuce on 27/12/13.
+//  Copyright (c) 2013 YUCE. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+- (id)initWithSize:(CGSize)size won:(BOOL)won;
+
+@end
+
+
